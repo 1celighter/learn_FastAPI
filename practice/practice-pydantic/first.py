c00db86535d9fb46df660fb68b3fca7f71e89a18
Nikita <petrovskiynikita22@gmail.com>
2026-07-04 21:37:36 +0300

    updated my first app to CRUD app

--- practice/practice-pydantic/first.py
+++ practice/practice-pydantic/first.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import ConfigDict, Field, BaseModel, EmailStr
 import uvicorn 
 
@@ -7,8 +7,9 @@ app = FastAPI()
 Items = []
 
 class ItemSchema(BaseModel):
+    Item_Id: int = Field(ge=0)
     title: str = Field(min_length=4,max_length=20)
-    price: int = Field(ge=0, le=50)
+    price: int = Field(ge=1, le=50)
     category: str = Field(min_length=4, max_length=15) 
     email: EmailStr    
 
@@ -19,6 +20,7 @@ class ItemSchema(BaseModel):
           summary="Create items here")
 def CreateItem(CreateItem:ItemSchema) -> dict:
     Items.append({
+        "Item_Id": len(Items) + 1,
         "Title": CreateItem.title,
         "Price": CreateItem.price,
         "Category": CreateItem.category,
@@ -29,13 +31,47 @@ def CreateItem(CreateItem:ItemSchema) -> dict:
         "Item": CreateItem
     }
 
-@app.get("/Get_Items",
+@app.get("/Items",
          tags=["Items"],
          summary="Get all items if we have it")
 def GetItems():
     return {
         "Message": "There all items",
         "Items": Items
+    }
+
+@app.get("/Items/{Item_Id}",
+         tags=["Items"],
+         summary="Get any item by Item-Id")
+def GetItemById(ItemById: int) -> dict:
+    for item in Items:
+        if item["Item-Id"] == ItemById:
+            return {
+                "Message": "There ur item by id",
+                "Item": item
+            }
+    raise HTTPException(status_code=404, detail=f"We cant found this item by this {ItemById} Item-Id")
+
+@app.delete("/Delete_Items/{Item_Id}",
+            tags=["Items"],
+            summary="Delete items from store by Item-Id")
+def DeleteItem(Item_Id: int) -> dict:
+    Delete_Item = Items.pop(Item_Id - 1)
+    return {
+        "Message": f"Item with {Item_Id} id has been deleted",
+        "Deleted content": Delete_Item
+    }
+@app.put("/Update_Item/{Item_Id}",
+         tags=["Items"],
+         summary="Update some data by id")
+def UpdateItem(Item_Id: int, New_Item: ItemSchema) -> dict:
+    index = Item_Id - 1 
+    item_dict = New_Item.model_dump()
+    item_dict["Item_Id"] = Item_Id
+    Items[index] = item_dict    
+    return {
+        "Message": f"Item with {Item_Id} id has been updated",
+        "Updated Content": Items[index]
     }
 
 
